d6426adedd05963c000778f71b3169474f1f9000
GoodieHART <152899645+GoodieHART@users.noreply.github.com>
2026-03-13 00:25:14 +0100

    Day 1

---
+++ data_types.py
@@ -0,0 +1,17 @@
+# numbers
+num_int = 14
+
+num_float = 3.14
+
+num_complex = 4 + 3j
+
+print(type(num_complex))
+
+# strings
+my_name = "Ace"
+boolean_var = True | False
+list_var = ["Me", "Jesus", "Myself"] # lists start with square brackets
+tuple_var = ("Mom", "Dad", "Siblings", "Ace") # tuples cannot be modified in subsequent code, like we cant do 'pop', 'insert' etc.
+set_var = {5, 8, 63, 97} # sets in python are sets in normal maths
+
+print(my_name, boolean_var, set_var, list_var, tuple_var)
---
+++ hello.py
@@ -0,0 +1,15 @@
+print(3 + 4)
+print(3 * 4)
+print(3 // 4)
+print(3 ** 4)
+print(3 - 4)
+print(3 / 4)
+
+
+print("Ace")
+print("I am enjoying 30 days of python")
+
+print(type("me"))
+print(type(4 + 5j)) #why isn't it i?
+print(type(["Goodie","Nigeria"]))
+
