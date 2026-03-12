07a205038d9126b4d8d40c741357ca942423fa5e
GoodieHART <igbonugoodheart@gmail.com>
2026-03-13 00:25:14 +0100

    Day 1

---
+++ euclidean_distance.py
@@ -0,0 +1,17 @@
+# euclidean distance is found using pythagoras theorem
+# we need 4 inputs for x1, x2, y1, y2
+# we then negate x1 from x2 and y1 from y2
+# we then sum the inputs
+# find the sqrt of the sum
+x1 = 2
+x2 = 10
+y1 = 3
+y2 = 8
+
+# we define new variables x3 and y3 to hold the values of our computations
+x3 = (x2 - x1) ** 2 # (10-2)^2
+y3 = (y2 - y1) ** 2 # (8 - 3)^2
+
+# final answer variable 'z'
+z = (x3 + y3) ** 0.5
+print(z)
